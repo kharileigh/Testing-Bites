2bb1ae19d1103501fd12da0a25b544511df5951e
kharileigh <kharileighmiller@gmail.com>
2024-08-07 17:43:38 +0100

    Added present.py - testing TBC

---
+++ tests/test_present.py
@@ -0,0 +1,23 @@
+from lib.present import *
+
+
+
+
+
+
+
+
+
+# class Present:
+#     def __init__(self):
+#         self.contents = None
+
+#     def wrap(self, contents):
+#         if self.contents is not None:
+#             raise Exception("A contents has already been wrapped.")
+#         self.contents = contents
+
+#     def unwrap(self):
+#         if self.contents is None:
+#             raise Exception("No contents have been wrapped.")
+#         return self.contents
