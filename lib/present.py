f0ae154ffbbc540c967e27633e5c4006b6a632c8
kharileigh <kharileighmiller@gmail.com>
2024-08-08 11:08:47 +0100

    Tests for present.py - passed

--- lib/present.py
+++ lib/present.py
@@ -3,11 +3,15 @@ class Present:
         self.contents = None
 
     def wrap(self, contents):
+        # exception if call wrap function again a second time on contents already wrapped
         if self.contents is not None:
             raise Exception("A contents has already been wrapped.")
+        # wrap contents & store them
         self.contents = contents
 
     def unwrap(self):
+        # exception when call unwrap function and no contents are stored
         if self.contents is None:
             raise Exception("No contents have been wrapped.")
+        # returns if there is contents already store
         return self.contents
--- tests/test_present.py
+++ tests/test_present.py
@@ -1,23 +1,61 @@
+# pytest - has an utility to handle exceptions so must import
+import pytest
 from lib.present import *
 
 
+"""""
+---------- wrap an item
+---------- get it back when unwrapping
+"""
+def test_wrap_and_then_unwrap():
+    present = Present()
+    present.wrap(33)
+    assert present.unwrap() == 33
 
 
+"""
+---------- unwrap before wrapping
+"""
+def test_unwrap_without_wrapping():
+    present = Present()
+    with pytest.raises(Exception) as err:
+        present.unwrap()
+    error_message = str(err.value)
+    assert error_message == "No contents have been wrapped."
 
 
+""""
+--------- wrap already wrapped present
+"""
+def test_wrap_already_wrapped():
 
+    present = Present()
 
+    # - wrap present first
+    present.wrap(66)
 
-# class Present:
-#     def __init__(self):
-#         self.contents = None
+    # wrap line of code that generates exception error - caught here
+    with pytest.raises(Exception) as err:
 
-#     def wrap(self, contents):
-#         if self.contents is not None:
-#             raise Exception("A contents has already been wrapped.")
-#         self.contents = contents
+        # - wrap again to trigger exception
+        present.wrap(44)
 
-#     def unwrap(self):
-#         if self.contents is None:
-#             raise Exception("No contents have been wrapped.")
-#         return self.contents
+    # extract exception itself - by putting error in a variable - convert to string to get message 
+    error_message = str(err.value)
+    
+    # assert expected error message
+    assert error_message == "A contents has already been wrapped."
+    
+
+""""
+--------- wrap already - try to wrap again - first value is unchanged
+"""
+def test_wrapping_already_wrapped_preserves_value():
+    present = Present()
+
+    present.wrap(66)
+
+    with pytest.raises(Exception) as err:
+        present.wrap(44)
+    
+    assert present.unwrap() == 66
